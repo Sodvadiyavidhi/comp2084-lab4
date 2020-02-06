71aa650059d870a85b81a2f42f1c629a6c5b9bfd
Sodvadiyavidhi <200429730@student.georgianc.on.ca>
2020-02-06 16:06:42 -0500

    completed lab4

---
+++ PhoneApplication/Models/Manufacturer.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.ComponentModel.DataAnnotations;
+
+namespace PhoneApplication.Models
+{
+    public class Manufacturer
+    {
+        
+        
+
+            public Manufacturer()
+            {
+                ManufacturerName = "";
+                URL = "";
+                Founded = DateTime.Now;
+            
+
+        }
+        [Key] public int ManufacturerID { get; set; }
+            public string ManufacturerName { get; set; }
+            public DateTime Founded { get; set; }
+            public string URL { get; set; }
+        public List<Phone> PhoneList { get; internal set; }
+        
+    }
+    }
+
---
+++ PhoneApplication/Models/Phone.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+
+namespace PhoneApplication.Models
+{
+    public class Phone
+    {
+        public Phone()
+        {
+            DateReleased = DateTime.Now;
+            PhoneName = "";
+
+
+        }
+        [ForeignKey("Manufacturer")]
+        public int ManufacturerID { get; set; }
+        [Key] public int  PhoneID { get; set; }
+        public string PhoneName { get; set; }
+        public virtual Manufacturer Manufacturer { get; set; }
+        public double MSRP { get; set; }
+        public int ScreenSize { get; set; }
+        public DateTime DateReleased { get; set; }
+    }
+}
